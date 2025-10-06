48221872ad6d47728d2c07e9d0127f7136a0be31
MoonMan120 <97407506+MoonMan120@users.noreply.github.com>
2025-10-06 10:24:58 -0500

    Update Scores.jsx

--- src/pages/Scores.jsx
+++ src/pages/Scores.jsx
@@ -1,69 +1,76 @@
 import Scorecard from "../components/Scorecard";
 import { useState, useEffect } from "react";
-import { fetchLiveGames } from "../api/ApiFunctions.js";
+import { fetchLiveGames, fetchUser } from "../api/ApiFunctions";
 
 export default function Scores() {
-  const [liveGames, setLiveGames] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+  const [liveGames, setLiveGames] = useState();
+  const [user, setUser] = useState();
+  const date = new Date();
+  const dayOfWeek = date.getDay();
+
+  const getFavoriteGame = () => {
+    if (user) {
+      const favoriteGame = liveGames?.find(
+        (game) =>
+          game.awayTeam.id === user.favorite_team ||
+          game.homeTeam.id === user.favorite_team
+      );
+      console.log(favoriteGame);
+      return favoriteGame;
+    }
+  };
+
+  const sortFunction = (a, b) => {
+    if (a.status !== "completed") {
+      if (a.startDate > b.startDate) {
+        return 1;
+      }
+      if (b.startDate > a.startDate) {
+        return -1;
+      } else {
+        return 0;
+      }
+    } else {
+      return -1;
+    }
+  };
 
   useEffect(() => {
-    async function loadData() {
-      try {
-        setLoading(true);
-        const games = await fetchLiveGames();
-        console.log(`Live games: ${games}`);
-        setLiveGames(games);
-      } catch (err) {
-        console.error("Error in Scores:", err);
-        setError("Failed to load games.");
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    loadData();
+    fetchLiveGames(setLiveGames);
+    fetchUser(setUser);
   }, []);
 
-  if (loading) return <p>Loading games...</p>;
-  if (error) return <p className="error">{error}</p>;
+  const favoriteGame = getFavoriteGame();
+  const gamesList = liveGames?.filter(
+    (game) =>
+      game.awayTeam.id !== user?.favorite_team &&
+      game.homeTeam.id !== user?.favorite_team
+  );
+  gamesList?.sort(sortFunction);
 
   return (
     <>
       <h1 className="scores-header">Scores</h1>
-      {liveGames.length > 0 ? (
-        <ul className="games-list">
-          {liveGames.map((game) => (
-            <li key={game.id} className="game-card">
-              <h2>
-                {game.away_team} @ {game.home_team}
-              </h2>
-              <p>
-                {game.away_points ?? "-"}:{game.home_points ?? "-"}
-              </p>
-              <p>
-                {
-                  "take a loke at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString"
-                }
-                {new Date(game.start_date).toLocaleString("en-US", {
-                  weekday: "short",
-                  month: "short",
-                  day: "numeric",
-                  hour: "numeric",
-                  minute: "2-digit",
-                })}
-              </p>
-              <p>
-                {game.completed
-                  ? "Final"
-                  : `Week ${game.season_week} ${game.season_type}`}
-              </p>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p> No games retrieved.</p>
-      )}
+      {dayOfWeek === 0 ||
+        (dayOfWeek === 1 && (
+          <h3 className="scores-header">
+            Next week's games will be available Tuesday!
+          </h3>
+        ))}
+      <ul>
+        <div className="favorite-game">
+          {favoriteGame && (
+            <Scorecard game={favoriteGame} user={user}></Scorecard>
+          )}
+          {/* An error message might be good, but I'm leaving it out for now because it shows in the background every time it's loading. */}
+          {/* {!favoriteGame && (
+            <h4>Your favorite team isn't playing this week!</h4>
+          )} */}
+        </div>
+        {gamesList?.map((game) => (
+          <Scorecard game={game} user={user} key={game.id} />
+        ))}
+      </ul>
     </>
   );
 }
